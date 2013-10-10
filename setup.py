079ac67ea52a5b9e3d7f787a97bd8d1614112c6a
cooketho <cooketho@gmail.com>
2013-10-09 18:53:24 -0700

    made changes to setup.py and compiled em.pyx

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
+from setuptools import setup
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 
-ext_modules = [Extension("em", ["gbstools/em.pyx"])]
+try:
+    from Cython.Distutils import build_ext
+    CYTHON = True
+except ImportError:
+    CYTHON = False
+
+requires = []
+
+try:
+    import collections
+    collections.OrderedDict
+except AttributeError:
+    requires.append('ordereddict')
+
+cmdclass = {}
+ext_modules = []
+
+if CYTHON:
+    ext_modules += [Extension("em", ["gbstools/em.pyx"])]
+    cmdclass.update({'build_ext':build_ext})
+else:
+    ext_modules += [Extension("em", ["gbstools/em.c"])]
 
 setup(
     name='GBStools',
@@ -10,7 +31,7 @@ setup(
     author='Tom Cooke',
     author_email='cooketho@gmail.com',
     packages=['gbstools'],
-    cmdclass={'build_ext':build_ext},
+    cmdclass=cmdclass,
     ext_modules=ext_modules,
     license='LICENSE.txt',
     description='Bioinformatics tools for GBS',
@@ -20,6 +41,5 @@ setup(
         "pysam >= 0.7.5",
         "numpy >= 1.3.0",
         "scipy >= 0.7.0",
-        "Cython >= 0.17.4"
     ],
 )
